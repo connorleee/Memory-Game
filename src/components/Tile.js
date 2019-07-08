ab76a4b043881d3996992c2a8d19e2bf49b2136d
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-07-07 22:32:54 -0700

    randomized display order of cars

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react"
-
+import cars from "../cars.json"
 
 class Tile extends Component {
     state = {
         selectedImgs: [],
-        imgDisplayOrder: []
+        imgDisplayOrder: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        cars: cars
+    }
+
+    componentDidMount() {
+        this.rndDisplayOrder()
+    }
+
+    shuffle = (a) => {
+        let j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+
+    rndDisplayOrder = () => {
+        let shuffCars = this.shuffle(cars)
+        let newOrder = []
+        for (let i = 0; i < 12; i++) {
+            newOrder.push(shuffCars[i].id)
+        }
+        this.setState({ imgDisplayOrder: newOrder })
     }
 
     handleClick = event => {
@@ -17,7 +42,7 @@ class Tile extends Component {
                 <div className="row">
                     <div className="col-md-3">
                         <div className="card text-white bg-primary mb-3">
-                            <img src="https://newspress-mclaren.s3.amazonaws.com/images/hero/4546-03_McLaren+570S_NYlaunch.jpg" className="card-img" alt="" />
+                            <img src={this.state.cars[3].image} className="card-img" alt="" />
                         </div>
                     </div>
                 </div>
